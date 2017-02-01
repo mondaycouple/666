e732d639b45b9f84343807b3bb8bf59d635aa207
mondaycouple <315695713@qq.com>
2017-02-01 18:24:49 +0800

    1
    1

---
+++ src/components/react.jsx
@@ -0,0 +1,9 @@
+var React = require("react");
+
+var React = React.createClass({
+  render: function() {
+    return <h1>React</h1>;
+  }
+});
+
+module.exports = React;
---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+var React = require("react");
+var React = require("./components/react.jsx");
+
+React.render(<React/>, document.body);
